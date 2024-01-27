d46ebf58f0430a8c5b8abdbf6f816c1ff72d5682
Eren Sönmez <testuser@gmail.com>
2024-01-27 15:22:03 +0300

    Yayınevleri tablosu oluşturuldu

--- SQL_Uygulamaları_3.sql
+++ SQL_Uygulamaları_3.sql
@@ -5,3 +5,17 @@ CREATE TABLE Ýller
 		check(ilkodu > 0),
 	Ýl_Adý varchar (20)
 )
+
+CREATE TABLE Yayýnevleri
+(
+	Yayýnevi_No int PRIMARY KEY,
+	Yayýnevi_Adý varchar(20),
+	Yetkili varchar(20),
+	ÝlKodu int FOREIGN KEY REFERENCES Ýller (Ýlkodu),
+	Tel varchar(11)
+		constraint ck_tel
+		check(tel like ('380-%')),
+	webadresi varchar(50)
+		constraint ck_webadresi
+		check (webadresi like ('www.%') AND webadresi like ('%.com'))
+)
