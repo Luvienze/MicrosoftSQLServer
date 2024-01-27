fad5ba8ec0f43f98e9146d5fd1471f7f3db407a0
TestUser <testuser@gmail.com>
2024-01-27 11:30:56 +0300

    URUNLER Tablosu oluşturuldu

--- SQL_Uygulamaları_1.sql
+++ SQL_Uygulamaları_1.sql
@@ -13,3 +13,18 @@ CREATE TABLE OGRENCILER
 		constraint ck_alan
 		check(alan in ('FEN','TM','SOS'))
 )
+
+CREATE TABLE URUNLER
+(
+	urunno int identity(1,1) PRIMARY KEY,
+	urunad varchar(15),
+	uruntaným varchar(50),
+	maliyetfiyat money,
+	karoran float,
+	KDV float,
+	kolisekli varchar(10)
+		constraint ck_kolisekli
+		check (kolisekli in('ADET','KG','DESTE','LITRE','SERI')),
+	CONSTRAINT ck_karoran CHECK (karoran >= maliyetfiyat *0.05 AND karoran <= maliyetfiyat*0.25),
+	CONSTRAINT ck_kdv CHECK (kdv >= maliyetfiyat *0.03 AND kdv <= maliyetfiyat *0.25)
+)
