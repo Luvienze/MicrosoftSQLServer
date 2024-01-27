6c092834c350221e4258d548a911ff290bd89c1e
TestUser <testuser@gmail.com>
2024-01-27 11:35:14 +0300

    ARABALAR tablosu oluşturuldu

--- SQL_Uygulamaları_1.sql
+++ SQL_Uygulamaları_1.sql
@@ -28,3 +28,22 @@ CREATE TABLE URUNLER
 	CONSTRAINT ck_karoran CHECK (karoran >= maliyetfiyat *0.05 AND karoran <= maliyetfiyat*0.25),
 	CONSTRAINT ck_kdv CHECK (kdv >= maliyetfiyat *0.03 AND kdv <= maliyetfiyat *0.25)
 )
+
+CREATE TABLE ARABALAR
+(
+	aracno varchar(4) PRIMARY KEY
+		constraint ck_aracno
+		check(aracno like ('[ORF][0-9][0-9][0-9]')),
+	marka varchar(10)
+		constraint ck_marka
+		check(marka in ('OPEL','RENAULT','FORD')),
+	model varchar(10),
+	trafikyil date DEFAULT('2009'),
+		constraint ck_trafikyil
+		check(trafikyil >= '01.01.2000' AND trafikyil <= '01.01.2009'),
+	renk varchar(10),
+	yakit varchar(10)
+		constraint ck_yakit
+		check(yakit in('BENZIN','DIZEL','LPG','KARMA')),
+	kilo int
+)
