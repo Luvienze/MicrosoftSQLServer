0436c38d3be1fcca2e8cd69663488a22d7e2c1e3
Eren Sönmez <testuser@gmail.com>
2024-01-27 15:26:05 +0300

    Kitaplar Tablosu oluşturuldu

--- SQL_Uygulamaları_3.sql
+++ SQL_Uygulamaları_3.sql
@@ -19,3 +19,25 @@ CREATE TABLE Yayýnevleri
 		constraint ck_webadresi
 		check (webadresi like ('www.%') AND webadresi like ('%.com'))
 )
+
+CREATE TABLE Kitaplar
+(
+	Kitap_No int identity(1,1) PRIMARY KEY,
+	Kitap_Adý varchar(20)
+		constraint ck_kitap_adý
+		check(kitap_adý like ('%[^a-zA-Z]%')),
+	Yazar_Adý varchar(20)
+		constraint ck_yazar_adý
+		check (yazar_adý like ('%[^a-zA-Z]%')),
+	Birim_Fiyat money,
+	Basým_Yýlý date NOT NULL
+		constraint ck_basým_yýlý
+		check(basým_yýlý <= getDate())
+		DEFAULT(getDate()),
+	Kategori varchar(20)
+		constraint ck_kategori
+		check(kategori in ('Programlama','Grafik-Tasarým','Networkd', 'Genel', 'Veritabaný')),
+	Adet int
+		constraint ck_adet
+		check(adet >= 0)
+)
